cd2e9bcafaebdd6a404e01730ac357a1a9288513
SachaWildCode <sacha.loumachi@gmail.com>
2024-06-17 15:42:42 +0200

    add routerlink & routerlinkactive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppComponent],
+  imports: [RouterOutlet, AppComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
