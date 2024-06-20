477d44f634ba2cc9385d9ca9bd63f5928f195560
SachaWildCode <sacha.loumachi@gmail.com>
2024-06-20 13:06:22 +0200

    fix background

---
+++ src/app/components/login-form/login-form.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-form',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './login-form.component.html',
+  styleUrl: './login-form.component.scss',
+})
+export class LoginFormComponent {
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: new FormControl(''),
+      password: new FormControl(''),
+    });
+  }
+}
