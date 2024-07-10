77c5703dcb25f46af114292ea53f17cef50d4ff9
Ghomedith <miche.edith@yahoo.fr>
2024-07-10 11:17:28 +0200

    donations component fisrt commit

---
+++ src/app/components/donations/donations.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DonationsComponent } from './donations.component';
+
+describe('DonationsComponent', () => {
+  let component: DonationsComponent;
+  let fixture: ComponentFixture<DonationsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DonationsComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(DonationsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/donations/donations.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donations',
+  standalone: true,
+  imports: [],
+  templateUrl: './donations.component.html',
+  styleUrl: './donations.component.scss',
+})
+export class DonationsComponent {}
