e2aa463500fc11a1188a22ac2e2be60124e746fc
Ghomedith <miche.edith@yahoo.fr>
2024-07-11 13:09:31 +0200

    My first commit  donations2 component

---
+++ src/app/components/donations2/donations2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donations2',
+  standalone: true,
+  imports: [Donations2Component],
+  templateUrl: './donations2.component.html',
+  styleUrl: './donations2.component.scss',
+})
+export class Donations2Component {}
--- src/app/pages/user-page/user-page.component.ts
+++ src/app/pages/user-page/user-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Donations2Component } from '../../components/donations2/donations2.component';
 
 @Component({
   selector: 'app-user-page',
   standalone: true,
-  imports: [],
+  imports: [Donations2Component],
   templateUrl: './user-page.component.html',
   styleUrl: './user-page.component.scss',
 })
