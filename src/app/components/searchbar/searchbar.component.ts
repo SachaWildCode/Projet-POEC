2287321d90cb15da9f31b3e7a3810780829e6a62
SachaWildCode <sacha.loumachi@gmail.com>
2024-07-25 16:07:06 +0200

    fixes search bar

--- src/app/components/searchbar/searchbar.component.ts
+++ src/app/components/searchbar/searchbar.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faFilter } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-searchbar',
   standalone: true,
-  imports: [],
+  imports: [FaIconComponent],
   templateUrl: './searchbar.component.html',
   styleUrl: './searchbar.component.scss',
 })
-export class SearchbarComponent {}
+export class SearchbarComponent {
+  faFilter = faFilter;
+}
